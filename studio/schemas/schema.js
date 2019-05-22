1ce607ac59edc4bc431d4cfa341f425bc5f40ee4
Nick Lewis <nickeblewis@gmail.com>
2019-05-22 21:34:39 +0100

    Chnanges

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -1,13 +1,23 @@
 // First, we must import the schema creator
-import createSchema from 'part:@sanity/base/schema-creator'
-
+import createSchema from "part:@sanity/base/schema-creator";
 // Then import schema types from any plugins that might expose them
-import schemaTypes from 'all:part:@sanity/base/schema-type'
+import schemaTypes from "all:part:@sanity/base/schema-type";
+
+// We import object and document schemas
+import blockContent from "./blockContent";
+import category from "./category";
+import product from "./product";
+import gallery from "./gallery";
+import page from "./page";
+import post from "./post";
+import vendor from "./vendor";
+import productVariant from "./productVariant";
+
 
 // document schemas
 import author from './documents/author'
-import category from './documents/category'
-import post from './documents/post'
+//import category from './documents/category'
+//import post from './documents/post'
 import siteSettings from './documents/siteSettings'
 
 // Object types
@@ -17,26 +27,38 @@ import excerptPortableText from './objects/excerptPortableText'
 import mainImage from './objects/mainImage'
 import authorReference from './objects/authorReference'
 
+//import localeString from "./locale/String";
+//import localeText from "./locale/Text";
+//import localeBlockContent from "./locale/BlockContent";
+
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
   // We name our schema
-  name: 'blog',
+  name: "default",
   // Then proceed to concatenate our our document type
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
     // The following are document types which will appear
     // in the studio.
     siteSettings,
+    product,
+    gallery,
+    page,
     post,
-    category,
+    vendor,
     author,
     mainImage,
+    category,
+    // When added to this list, object types can be used as
+    // { type: 'typename' } in other document schemas
+    blockContent,
+    //localeText,
+    //localeBlockContent,
+    //localeString,
     authorReference,
+    productVariant,
     bodyPortableText,
     bioPortableText,
     excerptPortableText
-
-    // When added to this list, object types can be used as
-    // { type: 'typename' } in other document schemas
   ])
-})
+});
--- web.2/client-config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  sanity: {
-    projectId: process.env.GATSBY_SANITY_PROJECT_ID || 'p50z86om',
-    dataset: process.env.GATSBY_SANITY_DATASET || 'production'
-  }
-}

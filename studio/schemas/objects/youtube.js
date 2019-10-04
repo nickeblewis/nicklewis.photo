68d5ad51fcd810a326711e767c0ce01a03bd9f39
nicklewiskier <nick.lewis2@kier.co.uk>
2019-10-04 11:41:17 +0100

    more youtube

--- studio/schemas/objects/youtube.js
+++ studio/schemas/objects/youtube.js
@@ -1,3 +1,13 @@
+import React from 'react'
+import getYouTubeId from 'get-youtube-id'
+import YouTube from 'react-youtube'
+
+const Preview = ({value}) => {
+	const { url } = value
+	const id = getYouTubeId(url)
+	return (<YouTube videoId={id} />)
+}
+
 export default {
     name: 'youtube',
     type: 'object',
@@ -8,5 +18,11 @@ export default {
         type: 'url',
         title: 'YouTube video URL'
       }
-    ]
+    ],
+    preview: {
+        select: {
+            url: 'url'
+        },
+        component: Preview
+    }
   }
